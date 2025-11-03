fc16e2c4081725f5c8fc0c7ceca2872ac4436472
Rumi Aktar <ce.rumiakter@gmail.com>
2025-11-03 23:16:43 +0600

    update

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 
 
 const LeftSideNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('category.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
     return (
         <div>
             <h1 className="text-2xl font-medium">All Caterogy</h1>
+            <div>
+                {categories.map((category) => (
+                    <p key={category.id}>
+                        {category.name}
+                    </p>
+                ))}
+            </div>
         </div>
     );
 };
